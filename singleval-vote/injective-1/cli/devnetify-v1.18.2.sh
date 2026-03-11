0fb57a65ed298d1cb749f95717586893d0bdd9b0
Max <maxim@injectivelabs.org>
2026-03-12 00:03:28 +0400

    chore: fixing devnetify for singleval-vote on v1.18.2

--- singleval-vote/injective-1/cli/devnetify-v1.18.2.sh
+++ singleval-vote/injective-1/cli/devnetify-v1.18.2.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 INJECTIVED="docker run -it --rm \
-    -v $(pwd)/injective-1:/apps/data/injective-1 \
+    -v $(pwd)/injective-1/validators/0:/apps/data/injective-1 \
+    -v $(pwd)/injective-1/validators:/apps/data/validators \
+    -v $(pwd)/injective-1/instances:/apps/data/instances \
     -v $(pwd)/custom_overrides.yaml:/apps/data/custom_overrides.yaml:ro \
     injectivelabs/injective-core:v1.18.2 injectived"
 
-$INJECTIVED --home "./injective-1" devnetify --skip-confirmation --custom-overrides ./custom_overrides.yaml injective-1
+$INJECTIVED --home "./injective-1" devnetify \
+    --skip-confirmation \
+    -V /apps/data/validators \
+    -K /apps/data/instances/0/accounts.json \
+    --custom-overrides ./custom_overrides.yaml \
+    injective-1
--- singleval-vote/injective-1/cli/injectived-v1.18.2.sh
+++ singleval-vote/injective-1/cli/injectived-v1.18.2.sh
@@ -5,7 +5,7 @@ ulimit -n 120000
 docker run -it --rm \
     -p 26657:26657 \
     -p 10337:10337 \
-    -v $(pwd)/injective-1:/apps/data/injective-1 \
+    -v $(pwd)/injective-1/validators/0:/apps/data/injective-1 \
     injectivelabs/injective-core:v1.18.2 injectived \
     --log-level "info" \
     --home "./injective-1" \
