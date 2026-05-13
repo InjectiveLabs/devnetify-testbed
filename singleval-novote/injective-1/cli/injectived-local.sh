30a4b535f1ee524eece75fc06cd0f22e92c79af3
Dusan Brajovic <dbrajovic3@gmail.com>
2026-05-13 17:10:14 +0200

    chore: temp setup

--- singleval-novote/injective-1/cli/injectived-local.sh
+++ singleval-novote/injective-1/cli/injectived-local.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# ulimit -n 120000
+ulimit -n 120000
 
-injectived \
---log-level "info" \
---home "./injective-1/validators/0" \
-start
+#injectived --log-level "info" --home "./injective-1/validators/0" start --metrics-enable-metrics --metrics-enable-tracing --metrics-insecure true
+injectived --log-level "info" --home "./injective-1/validators/0" start
