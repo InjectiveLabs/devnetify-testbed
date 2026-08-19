8321344cd8a16c15f4497344775e176aca7475d9
Max <maxim@injectivelabs.org>
2026-08-19 15:02:41 +0400

    chore: bump singleval devnetify

--- singleval-novote/injective-1/cli/injectived-v1.20.3.sh
+++ singleval-novote/injective-1/cli/injectived-v1.20.3.sh
@@ -3,7 +3,10 @@
 # ulimit -n 120000
 
 docker run -it --rm \
+    -p 26657:26657 \
+    -p 10337:10337 \
     -v $(pwd)/injective-1:/apps/data/injective-1 \
-    injectivelabs/injective-core:v1.16.4 injectived \
+    injectivelabs/injective-core:v1.20.3 injectived \
+    --log-level "info" \
     --home "./injective-1/validators/0" \
-    ${*:-"query"}
+    start
